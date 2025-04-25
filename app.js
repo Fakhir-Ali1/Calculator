3a42f6362a927c90fa79bf4be19f3ea4f9f4dcbf
Fakhir-Ali1 <a3arhamwajahat@gmail.com>
2025-04-25 16:19:53 +0500

    upload calculator

---
+++ app.js
@@ -0,0 +1,20 @@
+function pres(e) 
+
+{ var a = document.getElementById("inp").value += e}
+
+function equal() 
+{ var a = document.getElementById("inp")
+        a.value = eval(a.value)
+}
+ 
+function dlt()
+{
+    var a = document.getElementById("inp")
+    a.value = a.value.slice(0,-1)
+}
+
+function aclear(){
+    var a = document.getElementById('inp')
+ a.value = " "
+
+}
